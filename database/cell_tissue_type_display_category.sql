53f4112f99aac8fa127bf462974b00b27a2d37eb
Venkat Malladi <vmalladi@stanford.edu>
2013-04-25 15:55:48 -0700

    Added in updates to the MEPages_celltissuetypedisplaycategory table.

---
+++ database/cell_tissue_type_display_category.sql
@@ -0,0 +1,12 @@
+insert into MEPages_celltissuetypedisplaycategory values (1,'Skeletal system',1);
+insert into MEPages_celltissuetypedisplaycategory values (2,'Muscluar system',2);
+insert into MEPages_celltissuetypedisplaycategory values (3,'Cirulatory system',3);
+insert into MEPages_celltissuetypedisplaycategory values (4,'Nervous system',4);
+insert into MEPages_celltissuetypedisplaycategory values (5,'Respiratory system',5);
+insert into MEPages_celltissuetypedisplaycategory values (6,'Digestive system',6);
+insert into MEPages_celltissuetypedisplaycategory values (7,'Excretory system',7);
+insert into MEPages_celltissuetypedisplaycategory values (8,'Endocrine system',8);
+insert into MEPages_celltissuetypedisplaycategory values (9,'Reporductive system',9);
+insert into MEPages_celltissuetypedisplaycategory values (10,'Lyphatic system',10);
+insert into MEPages_celltissuetypedisplaycategory values (11,'Stem cells',11);
+
