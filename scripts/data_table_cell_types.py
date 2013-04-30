499baac01623fae8b4af868e3e0acbeeb178ce82
Venkat Malladi <vmalladi@stanford.edu>
2013-04-29 17:12:48 -0700

    Added in new data table cell tissue type groupings.

---
+++ scripts/data_table_cell_types.py
@@ -0,0 +1,8 @@
+i = 1
+ids= 301
+while i < 87:
+    print "insert into MEPages_datatablecelltissuetype values(",ids,",",i,",","1,0);"
+    ids +=1
+    print "insert into MEPages_datatablecelltissuetype values(",ids,",",i,",","2,0);"
+    ids +=1
+    i +=1
