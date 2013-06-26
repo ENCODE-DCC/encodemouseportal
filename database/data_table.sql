7bc2ebc272993ced1b3bea27df74dc1b379e1dcc
Venkat Malladi <vmalladi@stanford.edu>
2013-06-26 14:52:50 -0700

    updated data tables to be more visualing appealing."

---
+++ database/data_table.sql
@@ -0,0 +1,4 @@
+insert into MEPages_datatable values (1, 'Nontranscription Assays', 'nontranscription', 2, 100, 544, 485, 'datatable_headers/data_table_header_nontranscription.png', 434, 102);
+insert into MEPages_datatable values (2, 'Transcription Assays 1', 'transcription1', 2, 100, 856, 797, 'datatable_headers/data_table_header_transcription.png', 737, 102);
+insert into MEPages_datatable values (3, 'Transcription Assays 2', 'transcription2', 2, 100, 856, 797, 'datatable_headers/data_table_header_transcription.png', 737, 102);
+
